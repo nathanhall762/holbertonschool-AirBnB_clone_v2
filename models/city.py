9c1de196e5bd8cca6f1ce6c41c6a45ceb49d2f9e
Nathen Williams <nathenwilliams600@gmail.com>
2022-10-27 11:15:30 -0500

    Update City: (models/city.py)

--- models/city.py
+++ models/city.py
@@ -4,6 +4,19 @@ from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from models.base_model import Base, BaseModel
 
+#Update City: (models/city.py)
+
+#City inherits from BaseModel and Base (respect the order)
+#Add or replace in the class City:
+    #class attribute __tablename__ -
+        #represents the table name, cities
+    #class attribute name
+        #represents a column containing a string (128 characters)
+        #can’t be null
+    #class attribute state_id
+        #represents a column containing a string (60 characters)
+        #can’t be null
+        #is a foreign key to states.id
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
