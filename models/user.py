3ae03a8d2b5deb7d46e38c992d98f939312e7a08
Nathen Williams <nathenwilliams600@gmail.com>
2022-10-27 10:51:17 -0500

    7. DBStorage - User

--- models/user.py
+++ models/user.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 
+#Update User: (models/user.py)
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
