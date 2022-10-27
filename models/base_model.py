0e1d1eb4e4a063b23b3aa33c0799d45ececd10cb
Nathen Williams <nathenwilliams600@gmail.com>
2022-10-27 11:12:13 -0500

    6. DBStorage - States and Cities

--- models/base_model.py
+++ models/base_model.py
@@ -6,6 +6,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.sql.expression import null
 from sqlalchemy import Column, String, DateTime
 
+#Update BaseModel: (models/base_model.py)
 
 Base = declarative_base()
 
