4ae1787883a4db36ed297ac56cacd7d0d0a7358e
nathanhall762 <4943@holbertonstudents.com>
2022-11-15 11:23:39 -0600

    int 2:

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""documentation"""
+from flask import Flask
+
+
+flask_app = Flask(__name__)
+
+
+@flask_app.route('/', strict_slashes=False)
+def index():
+    """documentation"""
+    return 'Hello HBNB!'
+
+
+@flask_app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """documentation"""
+    return 'HBNB'
+
+
+@flask_app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """documentation"""
+    return 'C' + text.replace('_', ' ')
+
+
+if __name__ == '__main__':
+    flask_app.run('0.0.0.0')
