c9347827ee1483f58c4507f64457eafc75111083
nathanhall762 <4943@holbertonstudents.com>
2022-11-15 11:48:59 -0600

    try 3: move if name to top

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -3,33 +3,28 @@
 from flask import Flask
 
 
-flask_app = Flask(__name__)
-
-
-@flask_app.route('/', strict_slashes=False)
-def index():
-    """documentation"""
-    return 'Hello HBNB!'
-
-
-@flask_app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """documentation"""
-    return 'HBNB'
-
-
-@flask_app.route('/c/<text>', strict_slashes=False)
-def c(text):
-    """documentation"""
-    return 'C' + text.replace('_', ' ')
-
-
-@flask_app.route('/python/')
-@flask_app.route('/python/<text>', strict_slashes=False)
-def python(text="is cool"):
-    """documentation"""
-    return 'Python' + text.replace('_', ' ')
-
-
 if __name__ == '__main__':
+    flask_app = Flask(__name__)
+
+    @flask_app.route('/', strict_slashes=False)
+    def index():
+        """documentation"""
+        return 'Hello HBNB!'
+
+    @flask_app.route('/hbnb', strict_slashes=False)
+    def hbnb():
+        """documentation"""
+        return 'HBNB'
+
+    @flask_app.route('/c/<text>', strict_slashes=False)
+    def c(text):
+        """documentation"""
+        return 'C' + text.replace('_', ' ')
+
+    @flask_app.route('/python/')
+    @flask_app.route('/python/<text>', strict_slashes=False)
+    def python(text="is cool"):
+        """documentation"""
+        return 'Python' + text.replace('_', ' ')
+
     flask_app.run('0.0.0.0')
