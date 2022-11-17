7c5e134dfd86029bf1e70041b4faf6bfd4d40fd0
nathanhall762 <4943@holbertonstudents.com>
2022-11-17 10:53:39 -0600

    try 7:

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,6 +3,7 @@
 from flask import Flask
 from flask import render_template
 from models import storage
+from models.state import State
 
 
 flask_app = Flask(__name__)
@@ -12,12 +13,11 @@ flask_app = Flask(__name__)
 def states_list():
     """Display a HTML page of the States
     """
-    states = storage.all(State).values()
     return render_template('7-states_list.html', db=storage.all(State))
 
 
 @flask_app.teardown_appcontext
-def teardown_db(error):
+def teardown_db():
     """Closes the database again at the end of the request.
     """
     storage.close()
