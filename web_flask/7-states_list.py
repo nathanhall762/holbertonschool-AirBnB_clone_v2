af895fbb4bf95b7a6d3d18e5c411342b2c9eb3a6
nathanhall762 <4943@holbertonstudents.com>
2022-11-17 10:55:18 -0600

    fix 7: pld

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -11,15 +11,11 @@ flask_app = Flask(__name__)
 
 @flask_app.route('/states_list', strict_slashes=False)
 def states_list():
-    """Display a HTML page of the States
-    """
     return render_template('7-states_list.html', db=storage.all(State))
 
 
 @flask_app.teardown_appcontext
 def teardown_db():
-    """Closes the database again at the end of the request.
-    """
     storage.close()
 
 
