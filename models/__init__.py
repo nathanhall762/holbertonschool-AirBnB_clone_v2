ad55259c59b2bdf1d34c83f9c1d07fbc06d1b86f
nathanhall762 <4943@holbertonstudents.com>
2022-11-17 11:00:14 -0600

    fix 7: pld

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-
 from os import environ
 from models.base_model import BaseModel
 from models.user import User
@@ -10,7 +9,6 @@ from models.place import Place
 from models.review import Review
 
 
-# check envirn var to determine storage method
 if environ['HBNB_TYPE_STORAGE'] == 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
